2f2e2ae6ac7fe5e9efe6c020a0fc03be50c78e34
gabriellelyons <80489169+gabriellelyons@users.noreply.github.com>
2021-03-22 22:27:58 -0400

    Create PlayerActions.java

---
+++ Volleyball/PlayerActions.java
@@ -0,0 +1,10 @@
+//Gabrielle Lyons - CIS 1968, Spring 2020
+
+public interface PlayerActions {
+	public void pass();
+	public void set();
+	public void hit();
+	public void serve();	
+	public void doubleTouch();
+	public void miss();
+}
